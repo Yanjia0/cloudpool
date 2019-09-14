ef8e9b09db7c75a62b415e276e94b0d428b72285
Yanjia0 <Yanjia0@users.noreply.github.com>
2019-09-14 15:37:36 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,4 +9,16 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  //事件处理函数
+  bindViewTap: function(){
+
+  },
+  onLload: function(){
+
+  },
+  icon: function(e){
+    wx.makePhoneCall({
+      phoneNumber:'13575098615',
+    });
+  }
 })
