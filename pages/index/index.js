63c03f86dee4f9af3d580c3d228b706d17899d62
Yanjia0 <Yanjia0@users.noreply.github.com>
2019-09-15 11:25:34 +0800

    代码版
    1.将原代码中的数据放到了js的data里面，使得代码更简洁
    2.上一个版本遇到的真机调试图片不显示没有得到解决，虽然微信大发慈悲给了我一只猫，但还有三只被困在数据虚无的海洋里

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,9 +2,148 @@
 //获取应用实例
 const app = getApp()
 
+  // 是由一群年轻人管理的博客
+  // 文章、图片等来源于朋友们自愿提交
+  // 如果你也有兴趣加入我们
+  // 或者给我们提意见
+  // 欢迎联系我们
+  // cloudpool@zhuochi.org
 Page({
   data: {
-    motto: 'Hello World',
+    zhuyearray: [
+     [
+      {
+        str: 'CloudPool浊池',
+        styleclass: 'textH1'
+      }, 
+      {
+        str: '是由一群年轻人管理的博客',
+        styleclass: ''
+      }, 
+      {
+        str: '文章、图片等来源于朋友们自愿提交',
+        styleclass: ''
+      }, 
+      {
+        str: '如果你也有兴趣加入我们',
+        styleclass: ''
+      }, 
+      {
+        str: '或者给我们提意见',
+        styleclass: ''
+      }, 
+      {
+        str: '欢迎联系我们',
+        styleclass: ''
+      }, 
+      {
+        str: 'cloudpool@zhuochi.org',
+        styleclass: ''
+      }, 
+       ],
+      // <text class="textH1">为什么我们喜欢一个不存在的网站</text>
+      // <text>2017-01-07</text>
+      // <text class="textH1">为什么我们要做浊池</text>
+      // <text>2017-01-10</text>
+      // <text class="textH1">【不想手动置顶】(滑稽) 浊池需要帮助</text>
+      // <text>2099-01-01</text>
+       [
+         {
+           str: '为什么我们喜欢一个不存在的网站',
+           styleclass: 'textH1'
+         },
+         {
+           str: '2017-01-07',
+           styleclass: ''
+         }, 
+         {
+           str: '为什么我们要做浊池',
+           styleclass: 'textH1'
+         }, 
+         {
+           str: '2017-01-10',
+           styleclass: ''
+         }, 
+         {
+           str: '【不想手动置顶】(滑稽) 浊池需要帮助',
+           styleclass: 'textH1'
+         }, 
+         {
+           str: '2099-01-01',
+           styleclass: ''
+         }, 
+       ],
+       [
+         {
+           str: '忘了发出来的微电影（大概算吧）',
+           styleclass: 'textH1'
+         },
+         {
+           str: '2018-06-08',
+           styleclass: ''
+         }
+       ],
+      [
+        {
+          str: '关于物理的文章01',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2018-05-13',
+          styleclass: ''
+        },
+        {
+          str: '关于物理的文章02',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2018-05-13',
+          styleclass: ''
+        },
+        {
+          str: '奥卡姆上帝',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2018-08-26',
+          styleclass: ''
+        }
+      ],
+      [
+        {
+          str: '自用梯子教程',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2019-06-22',
+          styleclass: ''
+        },
+        {
+          str: '软件推荐v2.1',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2019-08-10',
+          styleclass: ''
+        },
+        {
+          str: 'WTG--U盘内安装Windows',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2019-01-21',
+          styleclass: ''
+        },
+        {
+          str: 'Windows安装',
+          styleclass: 'textH1'
+        },
+        {
+          str: '2018-10-17',
+          styleclass: ''
+        }
+      ]
+    ],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
