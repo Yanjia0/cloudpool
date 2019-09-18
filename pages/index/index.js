7e20730842534fccba0a9bcef24525737a91bb3f
Yanjia0 <Yanjia0@users.noreply.github.com>
2019-09-18 21:24:47 +0800

    图片正常版本
    解决了图片在真机调试时部分不显示的问题
    错误原因：png不能大写为PNG且图片本身命名的时候也要是png
    编辑器大小写都认，但是真机只认小写
    （现在我有四只猫了真开心）

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,6 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
-  // 是由一群年轻人管理的博客
-  // 文章、图片等来源于朋友们自愿提交
-  // 如果你也有兴趣加入我们
-  // 或者给我们提意见
-  // 欢迎联系我们
-  // cloudpool@zhuochi.org
 Page({
   data: {
     zhuyearray: [
@@ -41,12 +34,6 @@ Page({
         styleclass: ''
       }, 
        ],
-      // <text class="textH1">为什么我们喜欢一个不存在的网站</text>
-      // <text>2017-01-07</text>
-      // <text class="textH1">为什么我们要做浊池</text>
-      // <text>2017-01-10</text>
-      // <text class="textH1">【不想手动置顶】(滑稽) 浊池需要帮助</text>
-      // <text>2099-01-01</text>
        [
          {
            str: '为什么我们喜欢一个不存在的网站',
